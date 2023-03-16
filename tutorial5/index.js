368ca79a0852760db2cb08cec7c89f5cb62af532
karansinghrathore <karanrj31@gmail.com>
2023-03-16 17:16:42 -0300

    Update index.js

--- tutorial5/index.js
+++ tutorial5/index.js
@@ -7,69 +7,45 @@ const fileName = 'data.json';
 // https://expressjs.com/en/api.html
 app.use(express.json()); 
 
+let allUsersData = {users:[{email:"abc@abc.ca",firstName:"ABC",id:"5abf6782"},{id:"5abf6783",email:"emailemail@email.com",firstName:"Alexsss"},{id:"d6aeaac",email:"email2@email.com",firstName:"James"},{id:"j4i7ja",email:"email3@email.com",firstName:"Maya"}]}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.get('/users', (req, res) => {
-    const fs = require('fs')
-    const path = './'+ fileName;  
-    const fileData = JSON.parse(fs.readFileSync(path,'utf8'));
-    res.status(200).json({message : 'Users retrieved', success : true, users: fileData.users}); 
+    res.status(200).json({message : 'Users retrieved', success : true, users: allUsersData.users});
 });
 
 app.get('/user/:id', (req, res) => {
-    const fs = require('fs')
-    const path = './'+ fileName;  
-    const fileData = JSON.parse(fs.readFileSync(path,'utf8'));
+
     var userIdReceived = req.params['id'];
-    var usersData = fileData.users;
+    var usersData = allUsersData.users;
     const userIndex = usersData.findIndex(({ id }) => id === userIdReceived);
     if(userIndex == -1)
     {
-        res.status(404).json({message : 'User not found', success : false}); 
+        res.status(404).json({message : 'User not found', success : false});
     }
-    
-    res.status(200).json({ success : true, user: usersData[userIndex]}); 
+
+    res.status(200).json({ success : true, user: usersData[userIndex]});
 });
 
 app.post('/add', function(req,res){
-
-    const path = './'+ fileName;
-    const fs = require('fs');
-    const fileData = JSON.parse(fs.readFileSync(path,'utf8'));
-    var usersData = fileData.users;
     var randomId = Math.random().toString(20).substring(7);
-    usersData.push({id:randomId, email:req.body.email, firstName: req.body.firstName});
-    const usersJSONString = JSON.stringify({"users": usersData});
-    console.log(usersJSONString);
-    fs.writeFileSync(path, usersJSONString);
-    res.status(200).json({message : 'User added', success : true}); 
+    allUsersData.users.push({id:randomId, email:req.body.email, firstName: req.body.firstName});
+    res.status(200).json({message : 'User added', success : true});
 });
 
 
 
 app.put('/update/:id', (req, res) => {
     var userIdReceived = req.params['id'];
-    console.log("id received for put request is "+userIdReceived);
-    console.log("request body received is "+ req.body);
-    const fs = require('fs')
-    const path = './'+ fileName;  
-    const fileData = JSON.parse(fs.readFileSync(path,'utf8'));
-    var usersData = fileData.users;
-    const userIndex = usersData.findIndex(({ id }) => id === userIdReceived);
+    const userIndex = allUsersData.users.findIndex(({ id }) => id === userIdReceived);
     if(userIndex == -1)
     {
-        res.status(404).json({message : 'User not found', success : false}); 
+        res.status(404).json({message : 'User not found', success : false});
     }
-    usersData = usersData.slice(userIndex, userIndex+1);
+    allUsersData.users.splice(userIndex,1);
     console.log("index found is "+userIndex);
     const updatedUser = {"id": userIdReceived, "email":req.body?.email, "firstName": req.body?.firstName};
-    usersData.push(updatedUser);
-    const usersJSONString = JSON.stringify({"users": usersData});
-    console.log(usersJSONString);
-    fs.writeFileSync(path, usersJSONString);
+    allUsersData.users.push(updatedUser);
     res.status(200).json({message : 'User updated', success : true}); 
 });
 
